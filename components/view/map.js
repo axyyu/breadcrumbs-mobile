139d7d25fa0f6ff07f42778e2a8f558ff601a61a
Andrew Wang <andrewxiaoyu0@gmail.com>
2018-04-07 17:44:36 -0400

    Map is working

--- components/view/map.js
+++ components/view/map.js
@@ -2,21 +2,50 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
+import MapView from 'react-native-maps';
+
 export default class ItemMap extends React.Component {
+    static navigationOptions = {header:null};
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={styles.map}
+        initialRegion={{
+          latitude: 38.9937392,
+          longitude: -76.9470219,
+          latitudeDelta: 1,
+          longitudeDelta: 1,
+        }}
+/>
       </View>
     );
   }
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'blue',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
