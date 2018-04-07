065e6a9cc32278130b392e1b801b2eefbf9dabb0
Andrew Wang <andrewxiaoyu0@gmail.com>
2018-04-07 18:25:37 -0400

    Map is ok

--- components/view/map.js
+++ components/view/map.js
@@ -6,16 +6,46 @@ import MapView from 'react-native-maps';
 
 export default class ItemMap extends React.Component {
     static navigationOptions = {header:null};
+    constructor(props){
+        super(props);
+        this.state=this.getInitialState();
+    }
+    getInitialState() {
+      return {
+        "region": {
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        },
+      };
+    }
+    componentDidMount(){
+        navigator.geolocation.getCurrentPosition((pos)=>{
+            let coord = pos.coords;
+
+            this.onRegionChange({
+                latitude: coord.latitude,
+                longitude: coord.longitude,
+                latitudeDelta: 0.001,
+                longitudeDelta: 0.001,
+            });
+        }, (pos_err)=>console.log(pos_err));
+    }
+    onRegionChange(region) {
+        this.setState({ region });
+    }
   render() {
     return (
       <View style={styles.container}>
       <MapView style={styles.map}
-        initialRegion={{
-          latitude: 38.9937392,
-          longitude: -76.9470219,
-          latitudeDelta: 1,
-          longitudeDelta: 1,
-        }}
+        showsUserLocation={true}
+        showsMyLocationButton={false}
+        zoomEnabled = {true}
+        showsBuildings = {true}
+        onRegionChange={this.onRegionChange.bind(this)}
+        onRef={ref => (ref.animateToViewingAngle(65, 60000))}
+        initialRegion={this.state.region}
 />
       </View>
     );
